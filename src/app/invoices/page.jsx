98e90a4f92bfebd5f08c6dd6cacabe02c60db3eb
hanifmohammad <thenanifmohammad@gmail.com>
2025-03-15 19:26:28 +0530

    git push origin man

---
+++ src/app/invoices/page.jsx
@@ -0,0 +1,15 @@
+
+import Invoice from "@/components/section/Invoice";
+
+export const metadata = {
+  title: "fairlancer - Freelance Marketplace React/Next Js Template | Invoices",
+};
+
+export default function page() {
+  return (
+    <>
+    
+      <Invoice />
+    </>
+  );
+}
